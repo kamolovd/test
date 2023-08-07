07903f095615d56a7f455493b78800b70011de6d
root <daler.kamolov.19@mail.ru>
2023-08-07 16:59:50 +0500

    test

---
+++ precache-manifest.8ac6d048f998cb45cf3c4fbcb983eb8b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a02c3ec4a67456ef931f2a52e064df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da7210144b49f0e00322",
+    "url": "/static/css/2.88aaca39.chunk.css"
+  },
+  {
+    "revision": "00de67207d03acc2cc87",
+    "url": "/static/css/main.5280f2fc.chunk.css"
+  },
+  {
+    "revision": "da7210144b49f0e00322",
+    "url": "/static/js/2.3418c319.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/static/js/2.3418c319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00de67207d03acc2cc87",
+    "url": "/static/js/main.675b6af4.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "4f3dcc39fb18bda71249086268d8be38",
+    "url": "/static/media/android.4f3dcc39.svg"
+  },
+  {
+    "revision": "0146ad0ff1cfdcd3cce624c888cd4239",
+    "url": "/static/media/facebook.0146ad0f.svg"
+  },
+  {
+    "revision": "72e6d9f4cea0b3fb6a127db846aac0a2",
+    "url": "/static/media/hm.72e6d9f4.png"
+  },
+  {
+    "revision": "815dcaf690a235f1748f5262cf855102",
+    "url": "/static/media/instagramm.815dcaf6.svg"
+  },
+  {
+    "revision": "93980578f3a684b0a576de12fa993593",
+    "url": "/static/media/ios.93980578.svg"
+  },
+  {
+    "revision": "e3b43daa3f5ccdd24595bb8459dc7ee1",
+    "url": "/static/media/logo.e3b43daa.svg"
+  },
+  {
+    "revision": "84b9250162b6c2086ca524ab293bc0d8",
+    "url": "/static/media/next.84b92501.svg"
+  },
+  {
+    "revision": "8bc4e34f531777e59353b8c4a0206a53",
+    "url": "/static/media/next_white.8bc4e34f.svg"
+  },
+  {
+    "revision": "7a630425d075172b6af0a83f2bb85c06",
+    "url": "/static/media/paint.7a630425.svg"
+  },
+  {
+    "revision": "b23c8d2a0b49492435bfce006f57d799",
+    "url": "/static/media/productCont.b23c8d2a.png"
+  },
+  {
+    "revision": "d7bf74c0954d9b94f15eaa58cc45e114",
+    "url": "/static/media/send-money.d7bf74c0.svg"
+  },
+  {
+    "revision": "764d5f89875aafb8766a9f1bb365ab1f",
+    "url": "/static/media/telegramm.764d5f89.svg"
+  },
+  {
+    "revision": "bc4e9cb9285a773e143e47d50db08374",
+    "url": "/static/media/transfer.bc4e9cb9.svg"
+  },
+  {
+    "revision": "dc20f10141276885a76f12cd31e5a9fd",
+    "url": "/static/media/vk.dc20f101.svg"
+  }
+]);
